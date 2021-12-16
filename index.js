1a2d88558b2a78b87e4208700a09c4259feb2220
Piron Evgeniy <85217377+piron-johny@users.noreply.github.com>
2021-12-16 21:17:30 +0200

    Рандомно добовляет класс active

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const container = document.querySelector('.container');
-const slides = document.querySelectorAll('.slide');
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
-container.addEventListener('click', onCickSlides)
+function sliderPlagin(activeSlide) {
+  
+  slides[activeSlide].classList.add('active');
 
-function onCickSlides(e) {
-  if (e.target.nodeName === 'DIV') {
-    revoveClassActive()
-    e.target.classList.add('active')
+  const container = document.querySelector('.container');
+  const slides = document.querySelectorAll('.slide');
+
+  container.addEventListener('click', onCickSlides)
+
+  function onCickSlides(e) {
+    if (e.target.nodeName === 'DIV') {
+      revoveClassActive()
+      e.target.classList.add('active')
+    }
+    slide.classList.add('active')
   }
-  slide.classList.add('active')
+
+  function revoveClassActive() {
+    slides.forEach(slide => slide.classList.remove('active'))
+  }
 }
 
-function revoveClassActive() {
-  slides.forEach(slide => slide.classList.remove('active'))
-}
+sliderPlagin(getRandomInt(5))
